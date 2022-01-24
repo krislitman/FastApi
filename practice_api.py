2182ede533788661226345d22a96c1d1840ed0b7
Kris Litman <kris.d.litman@gmail.com>
2022-01-23 21:20:13 -0500

    Added pydantic models and added a dog model, and POST request to create a new dog object using fastapi

--- practice_api.py
+++ practice_api.py
@@ -1,12 +1,37 @@
+from typing import Optional
 from fastapi import FastAPI
 from models import RandomObject
+from pydantic import BaseModel
+
+class Dog(BaseModel):
+    name: str
+    description: Optional[str] = None
+    age: int
+    cute: Optional[bool] = True
 
 app = FastAPI()
 
 @app.get("/")
-def index():
+def root():
     object1 = RandomObject("Kris", 1)
     return {
             "Name": object1.name,
             "Number": object1.number
-            }
+    }
+
+@app.get("/dogs/{dog_id}")
+def read_dog(dog_id: int, q: Optional[str] = None):
+    return {
+        "dog_id": dog_id,
+        "q": q
+    }
+
+@app.post("/dogs/")
+async def create_dog(dog_id: int, dog: Dog, q: Optional[str] = None):
+    result = {
+        "dog_id": dog_id,
+        **dog.dict()
+    }
+    if q:
+        result.update({"q": q})
+    return result
